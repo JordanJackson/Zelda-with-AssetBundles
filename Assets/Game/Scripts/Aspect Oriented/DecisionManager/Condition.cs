e34d4e5c229b6aa7b337edfee711d19b55208adf
Jordan Jackson <jordanpatrickjackson@gmail.com>
2017-04-10 19:14:17 -0400

    Revert "Initial Project Commit"
    This reverts commit 13afaa40e50dc7e37f58e7b375b0bb8ebbecfd24.

--- Assets/Game/Scripts/Aspect Oriented/DecisionManager/Condition.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-// a logical 'block' that can be evaluated
-abstract public class Condition : MonoBehaviour {
-
-	abstract public bool Evaluate();
-}
--- Assets/Game/Scripts/Aspect Oriented/DecisionManager/Response.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-abstract public class Response : MonoBehaviour {
-
-	abstract public void Execute ();
-}
